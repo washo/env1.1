b1cb8672409f3e94a935096af33551ea49ef0adc
washo <washo3@gmail.com>
2023-01-16 12:21:13 +0900

    modified page.tsx & h1 css

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import { NEXT_PUBLIC_SITE_TITLE } from './server-constants'
 import GoogleAnalytics from '../components/google-analytics'
 import styles from '../styles/page.module.css'
@@ -8,16 +9,29 @@ const RootPage = () => (
     <GoogleAnalytics pageTitle={NEXT_PUBLIC_SITE_TITLE} />
     <div className={styles.container}>
       <div>
-        <h2>Welcome!</h2>
-        <p>Your easy-notion-blog deployed successfully!</p>
-        <p>Have fun!</p>
+        <h2>
+          Notionブログシステム<p></p> Next.jsを使った超高速静的ブログ
+        </h2>
         <p>
-          easy-notion-blog powered by{' '}
-          <Link href="https://github.com/otoyo/easy-notion-blog">
-            otoyo/easy-notion-blog
-          </Link>
+          このサイトは、Notionで書いた記事が、VercelのNext.jsによる自動デプロイで構築されています
         </p>
+        <Image
+          src="https://images.unsplash.com/photo-1623321582832-60af95a6663e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2371&q=80"
+          alt="Xmas"
+          width={600}
+          height={300}
+          sizes="100vw"
+          style={{
+            width: '100%',
+            height: 'auto',
+          }}
+        />
+        <p>
+          Notionで公開している記事は、別ブログの下書きであったり、ちょっとしたメモを残したりする利用なので、恒常的なものではありません
+        </p>
+        <p>独自ドメインではCloudFlareと連携させてます</p>
       </div>
+
     </div>
   </>
 )
