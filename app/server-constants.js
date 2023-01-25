415614f413e0ea2de0c7c722a1d5000df4cd0653
washo <washo3@gmail.com>
2023-01-25 16:24:17 +0900

    環境変数修正

--- app/server-constants.js
+++ app/server-constants.js
@@ -3,10 +3,10 @@
 
 const NOTION_API_SECRET = process.env.NOTION_API_SECRET
 const DATABASE_ID = process.env.DATABASE_ID
-const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL
-const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
-const NEXT_PUBLIC_SITE_TITLE = 'easy-notion-blog'
-const NEXT_PUBLIC_SITE_DESCRIPTION = 'Easy to start your blog. You can write on your Notion.'
+const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL || `https://notion.washo3.com`
+const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || `G-FL8SGFLWFB`
+const NEXT_PUBLIC_SITE_TITLE = 'Notionメモ'
+const NEXT_PUBLIC_SITE_DESCRIPTION = 'Notionで公開している記事'
 const NUMBER_OF_POSTS_PER_PAGE = 10
 
 module.exports = {
